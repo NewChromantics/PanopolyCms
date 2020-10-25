8125306a557f906bee53b36f1efca0bb1f74fe8a
Graham Reeves <graham@grahamreeves.com>
2020-10-25 15:44:43 +0000

    Now catching file error and sending ping loop

--- NodeServer.js
+++ NodeServer.js
@@ -1,7 +1,7 @@
 const os = require( 'os' );
 const fs = require( 'fs' );
 const WebSocketModule = require('ws');
-const Pop = require('./Pop_PromiseQueue');
+const Pop = require('./PopApi');
 
 const Port = process.env.Port || 8888;	//	gr: needs to be int?
 const ArtifactFileTries = process.env.ArtifactFileTries || 10;
@@ -21,8 +21,53 @@ function GetNewArtifactName()
 	return "ABCD";
 }
 
+
+class TArtifact
+{
+	constructor(Name)
+	{
+		this.Name = Name;
+		this.Filename = `${ArtifactPath}/${this.Name}.PopCap`;
+		this.Error = null;
+		this.Stream = null;
+		
+		const Options = {};
+		Options.flags = 'wx';	//	x = fail if exists
+		this.Stream = fs.createWriteStream(this.Filename,Options);
+		this.Stream.on('error', this.OnError.bind(this) );
+	}
+	
+	OnError(Error)
+	{
+		console.log(`Artifact stream error ${Error}`);
+		this.Error = Error || 'Unspecified stream error';
+	}
+	
+	Write(Data)
+	{
+		//	file has errored!
+		if ( this.Error !== null )
+		{
+			console.log(`Error failed as file has failed ${this.Error}`);
+			throw this.Error;
+		}
+		this.Stream.write(Data);
+		//console.log(`Wrote x${Data.length}`);
+	}
+}
+
 async function OpenNewArtifactStream()
 {
+	let Error = null;
+	
+	function OnError(Error)
+	{
+		//	https://stackoverflow.com/questions/20864036/error-handling-on-createwritestream
+		//file.read();
+		console.log(`Open file error ${Error}`);
+	}
+	
+	
 	//	how many tries do we allow?
 	//	how do we do this better... we're also relying on the file system to be atomic
 	for ( let i=0;	i<ArtifactFileTries;	i++ )
@@ -30,14 +75,8 @@ async function OpenNewArtifactStream()
 		const Name = GetNewArtifactName();
 		try
 		{
-			const Filename = `${ArtifactPath}/${Name}.popcap`;
-			const Options = {};
-			Options.flags = 'wx';	//	x = fail if exists
-			const Stream = fs.createWriteStream(Filename,Options);
-			const Result = {};
-			Result.Name = Name;
-			Result.Stream = Stream;
-			return Stream;
+			const Artifact = new TArtifact(Name);
+			return Artifact;
 		}
 		catch(e)
 		{
@@ -55,10 +94,12 @@ async function OpenNewArtifactStream()
 
 function StartWebsocketClientThread(Client,Request)
 {
+	console.log(`StartWebsocketClientThread(${Client},${Request})`);
 	const ClientAddress = Request.socket.remoteAddress;
 
 	function Disconnect(Error)
 	{
+		console.log(`Disconnect(${Error})`);
 		//	if error, send hail mary message before disconnecting
 		if ( Error )
 		{
@@ -107,10 +148,16 @@ async function WebsocketClientThread(Client)
 	//	send "disclaimer"
 	//	wait for an auth message
 	//	open file with random handle
-	const Artifact = OpenNewArtifactStream();
+	const Artifact = await OpenNewArtifactStream();
+	{
+		const StreamInitMessage = {};
+		StreamInitMessage.CmsInit = true;
+		Artifact.Write(JSON.stringify(StreamInitMessage));
+	}
 	//	send back Artifact Name
 	{
 		const ArtifactMessage = {};
+		ArtifactMessage.Command = 'Hello';
 		ArtifactMessage.ArtifactName = Artifact.Name;
 		const ArtifactAck = await SendAndWaitForReply(ArtifactMessage);
 		//	if ArtifactAck != something error
@@ -118,18 +165,40 @@ async function WebsocketClientThread(Client)
 	
 	async function RecvLoop()
 	{
+		//	write everything we recieve into the file
 		while(Connected)
 		{
 			const Message = await RecvMessageQueue.WaitForNext();
-			Artifact.Stream.Write(Message);
+			Artifact.Write(Message);
+		}
+	}
+	
+	function GetPingMessage()
+	{
+		const Message = {};
+		Message.Command = 'Ping';
+		return Message;
+	}
+	
+	async function PingLoop()
+	{	
+		//	occasionally send status back, this also will let us know if socket disconnects
+		while(Connected)
+		{
+			await Pop.Yield(10*1000);
+			const Message = GetPingMessage();
+			Client.send( JSON.stringify(Message) );
 		}
 	}
 	
 	//	now absorb messages and write to file
-	//	(maybe needs to be another thread so we can send stuff
-	const ReadFinishedPromise = await RecvLoop();
-	//	occasionly send back meta
-	//	check for size limits & stop
+	const ReadLoopFinished = RecvLoop();
+	//	todo: check for size limits & stop (in ping loop?)
+	const PingLoopFinished = PingLoop();
 	
+	//	wait for a thread to exit (probbaly disconnected)
+	await Promise.race([ReadLoopFinished,PingLoopFinished]);
+	
+	return 'WebsocketClientThread finished';
 }
 
